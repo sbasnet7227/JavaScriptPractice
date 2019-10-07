e9e5b9277b79d28fa65214de5f6306f8dc2bbd32
sbasnet7227 <basnet_sunil@hotmail.com>
2019-10-07 01:23:27 -0700

    third commit

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-var userFirstName = prompt('What is your First Name?');
-var userLastName = prompt('What is your last Name?');
-var age = prompt('What is your age?');
-var fullName = userFirstName+ ' ' + ' ' + userLastName
+// var userFirstName = prompt('What is your First Name?');
+// var userLastName = prompt('What is your last Name?');
+// var age = prompt('What is your age?');
+// var fullName = userFirstName+ ' ' + ' ' + userLastName
 
-document.write('My name is ' + fullName +' '+ 'and '  );
+// document.write('My name is ' + fullName +' '+ 'and '  );
 
-// console.log ('My name is '+ userFirstName + userLastName);
-document.write ('My age is '+ age);
-alert('Thank you ')
+// // console.log ('My name is '+ userFirstName + userLastName);
+// document.write ('My age is '+ age);
+// alert('Thank you ')
+
+// AGE CALCULATOR
+
+var age = prompt('Enter your age?');
+var calc = age * 365.25;
+alert (calc);
+document.write ('You are ' + calc + ' ' + 'days alive');
