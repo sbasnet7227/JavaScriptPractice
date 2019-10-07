f57e721d33a4eb4e8e28cceeb91afed854704dc0
sbasnet7227 <basnet_sunil@hotmail.com>
2019-10-07 01:09:17 -0700

    second commit

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var userFirstName = prompt('What is your First Name?');
 var userLastName = prompt('What is your last Name?');
 var age = prompt('What is your age?');
+var fullName = userFirstName+ ' ' + ' ' + userLastName
 
-document.write('My name is ' + userFirstName + ' '+ userLastName +' '+ 'and '  );
+document.write('My name is ' + fullName +' '+ 'and '  );
 
-console.log ('My name is '+ userFirstName + userLastName);
+// console.log ('My name is '+ userFirstName + userLastName);
 document.write ('My age is '+ age);
+alert('Thank you ')
